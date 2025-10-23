13c1f8c4f58b449041493ce589d3e310fde19dc0
BertoLaDK <lucasmulberg@live.dk>
2025-10-23 19:11:24 +0200

    Initial Mod Creation

---
+++ src/main/java/dk/bertola/meltingsnowblocks/MeltingSnowBlocksDataGenerator.java
@@ -0,0 +1,11 @@
+package dk.bertola.meltingsnowblocks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MeltingSnowBlocksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
