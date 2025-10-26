fa25cc2e4fd27373bece5826d99015e061fd3a49
BertoLaDK <lucasmulberg@live.dk>
2025-10-26 20:09:27 +0100

    Improved better ticks add to SNOW_BLOCK

--- src/main/java/dk/bertola/meltingsnowblocks/mixin/BlocksMixin.java
+++ src/main/java/dk/bertola/meltingsnowblocks/mixin/BlocksMixin.java
@@ -1,7 +1,8 @@
 package dk.bertola.meltingsnowblocks.mixin;
 
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -9,28 +10,19 @@ import net.minecraft.block.MapColor;
 import net.minecraft.sound.BlockSoundGroup;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Slice;
-
-import static dk.bertola.meltingsnowblocks.MeltingSnowBlocks.LOGGER;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 
 @Mixin(Blocks.class)
 public class BlocksMixin {
-    @ModifyExpressionValue(
+    @Definition(id = "register", method = "Lnet/minecraft/block/Blocks;register(Ljava/lang/String;Lnet/minecraft/block/Block;)Lnet/minecraft/block/Block;")
+    @Definition(id = "Block", type = Block.class)
+    @Expression("register('snow_block', @(new Block(?)))")
+    @ModifyArg(
             method = "<clinit>",
-            at = @At(
-                    value = "INVOKE",
-                    target = "Lnet/minecraft/block/AbstractBlock$Settings;create()Lnet/minecraft/block/AbstractBlock$Settings;",
-                    ordinal = 195 //Instance of when the SNOW_BLOCK is using create()
-            )
+            at = @At("MIXINEXTRAS:EXPRESSION")
     )
-    private static AbstractBlock.Settings modifySnowBlockSettings(AbstractBlock.Settings original) {
-
-        return original
-                .mapColor(MapColor.WHITE)
-                .requiresTool()
-                .strength(0.2f)
-                .sounds(BlockSoundGroup.SNOW)
-                .ticksRandomly();
+    private static AbstractBlock.Settings modifySnowBlockSettings(AbstractBlock.Settings settings) {
+        return settings.ticksRandomly();
     }
 }
